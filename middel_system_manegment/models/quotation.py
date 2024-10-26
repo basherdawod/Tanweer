f26a60adaf86c2b9c6693512cab280a865fb5626
Amna A.alrheem <112886455+AmnaAalrheem@users.noreply.github.com>
2024-10-26 08:11:24 +0400

    Maintenance Report

--- middel_system_manegment/models/quotation.py
+++ middel_system_manegment/models/quotation.py
@@ -105,6 +105,7 @@ class MiddelQuotation(models.Model):
 
     invoice_id = fields.Many2one('account.move', string='Invoice')
     invoice_count = fields.Integer(string="Invoice Count", compute='_get_invoiced', tracking=True)
+    middel_contract_line_ids = fields.One2many('middel.contract.line','contract_id')
     #
     # def write(self, vals):
     #     for record in self:
@@ -269,12 +270,24 @@ class MiddelQuotation(models.Model):
         else:
             result = {'type': 'ir.actions.act_window_close'}
         return result
+        
     def action_create_maintenance(self):
+        maintenance_line = []
+        for data in self.middel_contract_line_ids:
+            product_lines = {
+                'product_id': data.product_id.id,
+                'description': data.description,
+                'quantity': data.quantity,
+                'price_total': data.price_total,
+            }
+            maintenance_line.append(Command.create(product_lines))
+
         maintenance_record = self.env['middel.contract'].create({
             'partner_id': self.partner_id.id, 
             'quotation_id': self.id,
             'middel_quotation_id': self.id,
             'area_id':self.state_id.id,
+            'middel_list_ids':maintenance_line,
             
            
         })
@@ -286,3 +299,5 @@ class MiddelQuotation(models.Model):
         #     'view_mode': 'form',
         #     'target': 'current', 
         # }
+
+    
