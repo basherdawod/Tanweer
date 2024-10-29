6590bac6e59e2347cddabc4d4686ca3daaca6baa
dev <abdo_basher2000@yahoo.com>
2024-10-29 08:03:46 +0200

    add petty cash

--- petty_cash_manager/__manifest__.py
+++ petty_cash_manager/__manifest__.py
@@ -7,20 +7,20 @@
 ###############################################################################
 {
     'name': 'Petty Cash Manager',
-    'version': '17.0',
+    'version': '17.0.1.0',
     'category': 'Accounting',
     'summary': 'Manage petty cash requests, approvals, and accounting',
     'depends': ['base', 'account', 'hr'],  # Include any required modules
     'data': [
         'data/sequence_views.xml',
         'security/ir.model.access.csv',
-        # 'security/rules.xml',
+        'security/petty_cash_security.xml',
+        'security/petty_cash_record_rules.xml',
         'views/petty_cash_views.xml',
         'views/petty_card_form.xml',
         'views/payment.xml',
         'views/submited.xml',
         'views/petty_cach_request_form.xml',
-        # 'views/pety_cash.xml',
         'report/pety_cash_template.xml',
     ],
     "application": True,
